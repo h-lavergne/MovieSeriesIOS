adac8bb4ac98b6f579b14c7265a71ea966bfd783
Hugo Lavergne <43877402+h-lavergne@users.noreply.github.com>
2021-08-18 10:38:34 +0200

    Add files via upload

---
+++ MovieSeries/MovieSeries/MovieSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSeriesApp.swift
+//  MovieSeries
+//
+//  Created by LAVERGNE Hugo on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
